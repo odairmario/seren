eb1aab677bdf905c5a4eabf96c9abd0613976dea
Odair M <odairmario45@gmail.com>
2022-07-06 23:39:08 -0300

    feat(SystemdNspawn): Add SystemdNspawn methods, propriety checkers and start run method

--- src/seren/systemdNspawn.py
+++ src/seren/systemdNspawn.py
@@ -6,7 +6,9 @@ Github: https://github.com/odairmario/seren
 Description: Systemd-nspawn module
 """
 
-# from subprocess import Popen
+import subprocess as sp
+from os import path
+import shutil
 
 __author__ = "Odair M."
 __copyright__ = "Odair M."
@@ -17,15 +19,90 @@ class SystemdNspawn(object):
 
     """SystemdNspawn represent a wrap to call systemd-nspawn."""
 
-    def __init__(self, target, **kwargs):
+    def __init__(self, target_value, **kwargs):
         """constructor of the class
 
         :param target: mount point
 
         """
-        self._target = target
+        self.target = target_value
+        self.systemd_nspawn = kwargs.get("nspawn_path","systemd-nspawn")
+        self.nspawn_arguments = kwargs.get("nspawn_arguments",["-D"])
 
-    def __enter__(self, type, value, traceback):
+        # Stdout and Stderr
+        self._stderr = None
+        self._stdout = None
+
+    @property
+    def nspawn_arguments(self):
+        """Systemd-nspawn arguments
+        :returns: TODO
+
+        """
+
+        return self._nspawn_arguments
+
+    @nspawn_arguments.setter
+    def nspawn_arguments(self, arguments:list):
+        """Setter for nspawn_arguments, must be a list
+
+        :arguments: TODO
+        :returns: TODO
+
+        """
+
+        if type(arguments) is list:
+            self._nspawn_arguments = arguments
+        else:
+            raise ValueError("Arguments is not a list!")
+
+    @property
+    def target(self):
+        """Target getter
+        :returns: target path a string
+
+        """
+
+        return self._target
+    @target.setter
+    def target(self, target_value):
+        """Setter for target atribute
+
+        :target_value: TODO
+        :returns: TODO
+
+        """
+
+        if path.isdir(target_value):
+            self._target = target_value
+        else:
+            raise ValueError("Target path %s dosen't exist or not is a directory.",target_value)
+
+    @property
+    def systemd_nspawn(self):
+        """systemd_nspawn getter
+        :returns: TODO
+
+        """
+
+        return self._systemd_nspawn
+    @systemd_nspawn.setter
+    def systemd_nspawn(self, nspawn):
+        """TODO: Docstring for systemd_nspawn.
+
+        :nspawn: TODO
+        :returns: TODO
+
+        """
+
+        if shutil.which(nspawn):
+            self._systemd_nspawn = nspawn
+        else:
+            raise Exception("systemd-nspawn not found")
+
+
+
+    def __enter__(self):
         """__enter__ method for WITH stament
 
         :param type: TODO
@@ -38,7 +115,7 @@ class SystemdNspawn(object):
         return self
 
     # TODO
-    def __exit__(self):
+    def __exit__(self, type, value, traceback):
         """Exit __exit____exi____exit__
         :returns: TODO
 
@@ -51,3 +128,17 @@ class SystemdNspawn(object):
         :returns: TODO
 
         """
+        command = [self.systemd_nspawn,*self.nspawn_arguments,*commands]
+
+        self._stdout = sp.PIPE
+        self._stderr = sp.PIPE
+        try:
+            err = sp.run(command,
+                    stdout=self._stdout,
+                    stderr=self._stderr,
+                    shell=True,
+                    check=True,
+                    text=True
+                    )
+        except Exception as e:
+            raise e("error code %s",err)
--- tests/test_systemdNspawn.py
+++ tests/test_systemdNspawn.py
@@ -1,3 +1,72 @@
-# import pytest
+import pytest
+from seren.systemdNspawn import SystemdNspawn
 
-# from seren.systemdnspawn import SystemdNspawn
+
+__author__ = "Odair M."
+__copyright__ = "Odair M."
+__license__ = "MIT"
+
+@pytest.fixture
+def target(tmp_path) -> str:
+    """Return a string to a tmp dir
+    :returns: TODO
+
+    """
+
+    return str(tmp_path.absolute())
+
+
+def test_SystemdNspawn_instance(target):
+    """Test whether the class SystemdNspawn can be instancied
+    :param tmp_path: fixture to generate a temporary path
+    :returns: TODO
+
+    """
+    assert SystemdNspawn(target)
+
+def test_SystemdNspawn_target_existence():
+    """Test wheter the class SystemdNspawn check if the target exists and is a directory.
+    :returns: TODO
+
+    """
+    with pytest.raises(ValueError):
+        sn = SystemdNspawn("")
+
+def test_SystemdNspawn_alternative_systemd_nspawn(target):
+    """Test wheter systemdNspawn class raise value if systemd-nspawn dosent exists
+
+    :target: TODO
+    :returns: TODO
+
+    """
+    with pytest.raises(Exception):
+        sn = SystemdNspawn(target,nspawn_path=target+"/nspawn")
+
+    # test default value
+    with SystemdNspawn(target) as sn:
+        assert sn.systemd_nspawn == "systemd-nspawn"
+
+def test_SystemdNspawn_WITH_stament(target):
+    """Test wheter the class SystemdNspawn can be instancied with WITH stament
+
+    :target: TODO
+    :returns: TODO
+
+    """
+    with SystemdNspawn(target) as sn:
+        assert target == sn.target
+
+def test_SystemdNspawn_nspawn_arguments(target):
+    """Test wheter class can be handle diferents arguments
+
+    :target: TODO
+    :returns: TODO
+
+    """
+    valid_arguments=["--bind={}".format(target)]
+    invalid_arguments="--bind={}".format(target)
+    with SystemdNspawn(target,nspawn_arguments=valid_arguments) as sn:
+        assert valid_arguments == sn.nspawn_arguments
+
+    with pytest.raises(ValueError):
+        sn = SystemdNspawn(target,nspawn_arguments=invalid_arguments)
